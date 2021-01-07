9434f28a202b0b11591dc1c55843646b8c2de204
jakbes <72144312+jakbes@users.noreply.github.com>
2021-01-07 10:48:20 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+"""Novolinehook application simulator, dont take it serious its just joke project"""
+
+
+print ("do you use or ever used fanta?")
+yesorno = input()
+if yesorno == "yes":
+    print ("go to hell fanta gay")
+elif yesorno == "no":
+    print ("you are accepted! now pay.")
+else:
+    print ("YOU ARE NOT ACCEPTED IF U WANNA BE ACCEPTED JUST ANSWER ME OR GO SUCK WITH iNIURIA AND GO LOSE AGAINST LEGITS")
